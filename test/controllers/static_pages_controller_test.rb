8d58ea8ec9a017b6d15a017d573f69aa336e1cd6
Adam Wysknski <sigmund.messner@gmail.com>
2016-09-30 13:13:18 +0200

    Define simple routes, add test tools

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,14 +1,23 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+
   test "should get home" do
     get static_pages_home_url
     assert_response :success
+    assert_select "title", "Home | Chirper"
   end
 
   test "should get help" do
     get static_pages_help_url
     assert_response :success
+    assert_select "title", "Help | Chirper"
+  end
+
+  test "should get about page" do
+    get static_pages_about_url
+    assert_response :success
+    assert_select "title", "About | Chirper"
   end
 
 end

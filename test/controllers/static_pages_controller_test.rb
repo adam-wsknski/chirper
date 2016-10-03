17d84d19f3f1d89f8bfb24273b3c88567b8228c4
Adam Wysknski <sigmund.messner@gmail.com>
2016-10-03 12:11:46 +0200

    Add contact page, improve routing to clean endpoints/paths

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -2,22 +2,22 @@ require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
 
-  test "should get home" do
-    get static_pages_home_url
-    assert_response :success
-    assert_select "title", "Home | Chirper"
-  end
-
   test "should get help" do
-    get static_pages_help_url
+    get help_path
     assert_response :success
     assert_select "title", "Help | Chirper"
   end
 
   test "should get about page" do
-    get static_pages_about_url
+    get about_path
     assert_response :success
     assert_select "title", "About | Chirper"
+  end
+
+  test "should get contact page" do
+    get contact_path
+    assert_response :success
+    assert_select "title", "Contact | Chirper"
   end
 
 end

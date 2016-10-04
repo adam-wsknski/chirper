f79a918984d1ee5e7adc1c4e4d1d07d6f61cef47
Adam Wysknski <sigmund.messner@gmail.com>
2016-10-04 09:49:26 +0200

    add flash messages after redirect, add proper test, tinker with styling

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -5,11 +5,29 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
   test "invalid signup info" do
     get signup_path
     assert_no_difference 'User.count' do
-      post users_path, user: { name: "",
-                               email: "user@invalid.pl",
-                               password: "lokos",
-                               password_confirmation: "locos"}
+      post users_path, params: { user:
+                                 { name: "",
+                                   email: "user@invalid.pl",
+                                   password: "lokos",
+                                   password_confirmation: "locos"
+                                  }
+                               }
     end
     assert_template 'users/new'
+  end
+
+  test "valid signup info" do
+    get signup_path
+    assert_difference 'User.count', 1 do
+      post users_path, params: { user:
+                                 { name: "Examp;",
+                                   email: "user@valid.pl",
+                                   password: "locos12",
+                                   password_confirmation: "locos12"
+                                  }
+                               }
+    end
+    follow_redirect!
+    assert_template 'users/show'
   end
 end
